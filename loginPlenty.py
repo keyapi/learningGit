07b8a6624ed0eb4251cfb928ea594ea7fabf2e4f
keyapi <43301887+keyapi@users.noreply.github.com>
2018-09-16 12:26:59 +0200

    login return

--- loginPlenty.py
+++ loginPlenty.py
@@ -19,7 +19,18 @@ def login(sellerUrl, username, password):
         re = requests.post(loginUrl)
         if re.status_code == 200:
             print ("Your login is successful!")
+			login_re_js = re.json()
+			token_type = login_re_js['token_type']
+			access_token = login_re_js['access_token']
+			refresh_token = login_re_js['refresh_token']
+			head = {'Authorization': token_type + " " + access_token}
+			
             return re
+			return login_re_js
+			return token_type
+			return access_token
+			return refresh_token
+			return head
             break
         else:
             print ("Your " + str(count) + ". login failed, please try again! (max. 3 tries)")

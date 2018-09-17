853482c79c23b7d715041dab99bf83db4995b2d0
keyapi <43301887+keyapi@users.noreply.github.com>
2018-09-17 11:40:35 +0200

    merge

--- loginPlenty.py
+++ loginPlenty.py
@@ -1,26 +1,30 @@
-import json
+import getpass
 import requests
-import getpass #no showing the input username and password
+import json
 
-sellerUrl = ""
-username = ""
-password = ""
+def loginInput():
+    print("Please enter your PM login data and press Enter button")
+    username = input("username: ")
+    password = getpass.getpass("password: ")
+    return username, password
 
-print ("Login")
-
-def login(sellerUrl, username, password):
+def loginPost(sellerUrl, username, password):
     sellerUrl = sellerUrl
+    username, password = loginInput()
     route = "/rest/login"
+    paras = f"?username={username}&password={password}"
+    loginUrl = sellerUrl + route + paras  
+    re = requests.post(loginUrl)
+    return re
+
+def loginTries(sellerUrl):
+    sellerUrl = sellerUrl
+    username = ""
+    password = ""
     count = 0
     while count < 3:
         count += 1
-        
-        username = input("username: ")
-        password = getpass.getpass("password: ")
-        paras = f"?username={username}&password={password}"
-        loginUrl = sellerUrl + route + paras
-        
-        re = requests.post(loginUrl)
+        re = loginPost(sellerUrl, username, password)
         if re.status_code == 200:
             print ("login successful")
             login_re_js = re.json()
@@ -30,21 +34,12 @@ def login(sellerUrl, username, password):
             head = {'Authorization': token_type + " " + access_token}
             
             return token_type, access_token, refresh_token, head
-            #return login_re_js
-            #return token_type
-            #return access_token
-            #return refresh_token
-            #return head
             break
         else:
             print ("Your " + str(count) + ". login failed, please try again! (max. 3 tries)")
     else:
         print ("Your 3 tries all failed, please contact admin!")
 
-#loginRe = login(sellerUrl, username, password)
-
-#return login_re_js = loginRe.json()
-#return token_type = login_re_js['token_type']
-#return access_token = login_re_js['access_token']
-#return refresh_token = login_re_js['refresh_token']
-#return head = {'Authorization': token_type + " " + access_token}
+	
+if __name__== "__main__":
+    loginTries(sellerUrl)
